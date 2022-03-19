ba4d78f130fdb49bb74bde19efab495a773860a5
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-03-19 15:37:40 +0330

    First Components and Props

---
+++ src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+<ul></ul>
+
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            storedResourses:[
+                {id : 'official-guide', title :'Official Guide', description : 'The Official Vue.js documentation.',
+                link: 'https://vuejs.org'
+                },
+                 {id : 'google', title :'Google', description : 'Learn To Google..',
+                link: 'https://google.org'
+                },
+                
+            ]
+        };
+    }
+}
+</script>
